cddb981cdc1e77d3f9a3fe40619f195a9e4bf289
Yemane Yohannes <74642492+yemaney@users.noreply.github.com>
2023-06-17 03:36:00 +0000

    [LeetCode Sync] Runtime - 49 ms (49.39%), Memory - 7.8 MB (48.90%)

--- 0448-find-all-numbers-disappeared-in-an-array/solution.go
+++ 0448-find-all-numbers-disappeared-in-an-array/solution.go
@@ -1,15 +1,44 @@
-func findDisappearedNumbers(nums []int) []int {
-    m := make(map[int]int, len(nums))
+// func findDisappearedNumbers(nums []int) []int {
+ 
+//     // map to hold found nums
+//     m := make(map[int]int, len(nums))
 
-    for _, v :=  range nums {
-        m[v]++
+//     // add each num in nums to map
+//     for _, v :=  range nums {
+//         m[v]++
+//     }
+
+//     // create a slice of all nums not found in the map 
+//     var msing []int
+//     for i, _ := range nums {
+//         if _, ok := m[i+1]; !ok {
+//             msing = append(msing, (i+1))
+//         }
+    
+//     return msing
+// }
+
+func findDisappearedNumbers(nums []int) []int {
+    missing := make([]int, len(nums))
+
+    // using result array as sorted array for seen values
+    for _, v := range nums {
+        missing[v-1] = v
     }
 
-    var msing []int
-    for i, _ := range nums {
-        if _, ok := m[i+1]; !ok {
-            msing = append(msing, (i+1))
+    // here we have res array where missing records will be with value = 0
+    // scan all values with 0 and "shift" them to head of res array
+    j := 0
+    for i, v := range missing {
+        if v == 0 {
+            missing[j] = i + 1
+            j++
         }
     }
-    return msing
+ 
+    // chop head of res array and return it
+    return missing[:j]
 }
+
+
+
