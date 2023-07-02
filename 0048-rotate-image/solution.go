0748487142feca546878b4f9cff042fa170b21f1
Yemane Yohannes <74642492+yemaney@users.noreply.github.com>
2023-07-02 23:10:04 +0000

    [LeetCode Sync] Runtime - 2 ms (20.99%), Memory - 2.2 MB (100.00%)

---
+++ 0048-rotate-image/solution.go
@@ -0,0 +1,15 @@
+func rotate(matrix [][]int)  {
+    l := len(matrix)
+
+    for i := 0; i < l; i++ {
+      for j := i+1; j < l; j++ {
+        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
+      }
+    } 
+
+    for i := 0; i < l; i++ {
+      for j:=0; j< l/2; j++{
+        matrix[i][j], matrix[i][l-j-1] = matrix[i][l-j-1], matrix[i][j] 
+      }
+    }
+}
